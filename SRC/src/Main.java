f3fe25e0683d26e014462af40ed4b37f3d4d58aa
Joao Felipe <joaolipepe11@gmail.com>
2026-03-24 16:43:28 -0300

    Codigo com CLASSES ENUM criadas

--- SRC/src/Main.java
+++ SRC/src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Usuario a1 = new Usuario("joao", "123", "senha", 20, "M", "email",
+        Usuario u = new Usuario("joao", "123", "senha", 20, "M", "email",
                 "Joao", "1111", "123");
 
-        Filme f1 = new Filme("Batman", 120, "Filme", 20.0);
+        Filme f = new Filme("Batman", 120, "Filme do Batman", 20.0);
 
-        Sessao s1 = new Sessao(f1, 1); 
-        s1.reservarCadeira(2, 5);
+        Sessao s = new Sessao(f, 1);
+        s.reservarCadeira(2, 5);
 
-        Bilhete b1 = new Bilhete(a1, s1, 2, 5);
+        Bilhete b = new Bilhete(u, s, 2, 5);
 
-        Compra c1 = new Compra();
-        c1.adicionarBilhete(b1);
+        Compra c = new Compra();
+        c.adicionarBilhete(b);
 
-        c1.mostrarCompra();
+        c.mostrarCompra();
     }
 }
