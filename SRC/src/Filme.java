9a27d1fac21a2fe77705b2e92afd8b3b4ae1a42e
JoaoFelipe005 <joaolipepe11@gmail.com>
2026-03-26 09:28:30 -0300

    Classe filme atualizada

--- SRC/src/Filme.java
+++ SRC/src/Filme.java
@@ -1,20 +1,26 @@
 public class Filme {
     private String nome;
-    private int duracao;
-    private String sinopse;
     private double valor;
+    private double nota;
+    private int quantidadeCriticos;
+    private Critica[] criticas = new Critica[100];
+    private int totalCriticas = 0;
 
-    public Filme(String nome, int duracao, String sinopse, double valor) {
+    public Filme(String nome, double valor) {
         this.nome = nome;
-        this.duracao = duracao;
-        this.sinopse = sinopse;
         this.valor = valor;
     }
 
-    public String getNome() { 
-        return nome; 
+    public void adicionarCritica(Critica c) {
+        criticas[totalCriticas] = c;
+        totalCriticas++;
     }
-    public double getValor() { 
-        return valor; 
+
+    public void adicionarNota(double novaNota) {
+        nota = ((nota * quantidadeCriticos) + novaNota) / (quantidadeCriticos + 1);
+        quantidadeCriticos++;
     }
+
+    public String getNome() { return nome; }
+    public double getValor() { return valor; }
 }
