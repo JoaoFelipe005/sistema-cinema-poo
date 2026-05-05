63f4c7dbb5eb164edcb7cfbf0a5a7d2da51285bd
JoaoFelipe005 <joaolipepe11@gmail.com>
2026-05-05 09:23:14 -0300

    Main com criticas ao Filme

--- SRC/src/Main.java
+++ SRC/src/Main.java
@@ -1,28 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        
         Usuario u = new Usuario("João", "123", "senha", 20, "M", "email",
                 "Joao", "1111", "123");
 
-        Estudante e = new Estudante("Maria", "456", "programaçao", 18, "F", "email",
+        Estudante e = new Estudante("Maria", "456", "programacao", 18, "F", "email",
                 "Maria", "2222", "456");
-             
-        Critico critico = new Critico("Leo Dias", "555", "fofoca", 45, "M", "email","leo", "1234", "554", "Globo");
 
-        Filme f = new Filme("Batman", 20.0);
-        Filme f2 = new Filme("Orgulho e Preconceito", 20.0);
+        Critico critico = new Critico("Leo Dias", "555", "fofoca", 45, "M", "email",
+                "leo", "1234", "554", "Globo");
+
+        Filme f = new Filme("Batman", 20.0, 120, "Filme do Batman");
+        Filme f2 = new Filme("Orgulho e Preconceito", 20.0, 130, "Romance clássico");
 
         Sessao s = new Sessao(f, 1, TipoSala.TRES_D);
-        s.reservarCadeira(2, 5);
-        Sessao s2 = new Sessao(f2, 2,TipoSala.COMUM);
-        s2.reservarCadeira(4, 7);
+        Sessao s2 = new Sessao(f2, 2, TipoSala.COMUM);
 
         Bilhete b1 = new Bilhete(u, s, 2, 5);
         Bilhete b2 = new Bilhete(e, s, 2, 6);
         Bilhete b3 = new Bilhete(critico, s2, 3, 4);
 
         Compra c = new Compra();
+
         c.adicionarBilhete(b1);
         c.adicionarBilhete(b2);
         c.adicionarBilhete(b3);
@@ -30,14 +29,39 @@ public class Main {
         c.adicionarProduto(Produto.PIPOCA);
         c.adicionarProduto(Produto.REFRIGERANTE);
 
-        double total = c.calcularTotal();
+        // CRÍTICAS ORGANIZADAS
+        
+        // Críticas para o filme Batman
+        critico.atribuirNota(10, f);
+        critico.atribuirCritica("Filme muito bom.", f);
 
-        double totalComDesconto = c.calcularTotal(CupomPromocional.DESCONTO10);
+        critico.atribuirNota(8, f);
+        critico.atribuirCritica("Ótima atuação do protagonista.", f);
 
+        // Críticas para o outro filme
+        critico.atribuirNota(7, f2);
+        critico.atribuirCritica("Bom, mas um pouco lento.", f2);
+
+
+        // EXIBIÇÃO
         c.mostrarCompra();
 
-        System.out.println("Total normal: R$ " + total);
-        System.out.println("Total com desconto: R$ " + totalComDesconto);
-        
+        System.out.println("\n===== FILME 1 =====");
+        System.out.println("Nome: " + f.getNome());
+        System.out.println("Nota média: " + f.getNota());
+        System.out.println("Quantidade de críticos: " + f.getQuantidadeCriticos());
+        System.out.println("Críticas:");
+        f.mostrarCriticas();
+
+        System.out.println("\n===== FILME 2 =====");
+        System.out.println("Nome: " + f2.getNome());
+        System.out.println("Nota média: " + f2.getNota());
+        System.out.println("Quantidade de críticos: " + f2.getQuantidadeCriticos());
+        System.out.println("Críticas:");
+        f2.mostrarCriticas();
+
+        System.out.println("\nTotal normal: R$ " + c.calcularTotal());
+        System.out.println("Total com desconto: R$ " +
+                c.calcularTotal(CupomPromocional.DESCONTO10));
     }
 }
