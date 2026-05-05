51aa180398bfffd81d9cf51e7a35209de05e99e1
JoaoFelipe005 <joaolipepe11@gmail.com>
2026-05-05 08:39:50 -0300

    Classe filme com notas e criticas

--- SRC/src/Filme.java
+++ SRC/src/Filme.java
@@ -1,12 +1,64 @@
 public class Filme {
     private String nome;
     private double valor;
-   
-    public Filme(String nome, double valor) {
+    private int duracao;
+    private String sinopse;
+
+    private double nota;
+    private int quantidadeCriticos;
+
+    private Critica[] criticas = new Critica[100];
+    private int qtdCriticas = 0;
+
+    public Filme(String nome, double valor, int duracao, String sinopse) {
         this.nome = nome;
         this.valor = valor;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.nota = 0;
+        this.quantidadeCriticos = 0;
     }
 
-    public String getNome() { return nome; }
-    public double getValor() { return valor; }
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public int getQuantidadeCriticos() {
+        return quantidadeCriticos;
+    }
+
+    // adicionar nota e recalcula a média
+    public void adicionarNota(double novaNota) {
+        nota = (nota * quantidadeCriticos + novaNota) / (quantidadeCriticos + 1);
+        quantidadeCriticos++;
+    }
+
+    // adicionar crítica no vetor
+    public void adicionarCritica(Critica critica) {
+        criticas[qtdCriticas] = critica;
+        qtdCriticas++;
+    }
+
+    // mostrar críticas
+    public void mostrarCriticas() {
+        for (int i = 0; i < qtdCriticas; i++) {
+            System.out.println(criticas[i].getMensagem());
+        }
+    }
 }
