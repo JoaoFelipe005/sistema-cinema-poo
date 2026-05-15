b28137809be483fbe040285082dc53d3209645b0
Joao Felipe <joaolipepe11@gmail.com>
2026-05-15 16:32:22 -0300

    Classe funcionario com metodo da classe Abstrata base

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -14,3 +14,15 @@ public class Funcionario extends Base implements GerenciaFilmes {
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
+    @Override
+    public void adicionarUsuario(Usuario usuario) { // metodo da classe abstrata
+        System.out.println("Usuário adicionado pelo funcionário.");
+    }
+
+    @Override
+    public void alterarUsuario(Usuario usuario) {
+        System.out.println("Usuário alterado pelo funcionário.");
+    }
+
+    }
