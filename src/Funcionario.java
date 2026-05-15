bb847f21768f54fb1ea3a9066ba3a8aab6218e93
Joao Felipe <joaolipepe11@gmail.com>
2026-05-15 15:47:26 -0300

    Comecando a classe Funcionario

---
+++ src/Funcionario.java
@@ -0,0 +1,16 @@
+public class Funcionario extends Base implements GerenciaFilmes {
+
+    private double salario;
+
+    public Funcionario(String nome, int idade, String email, double salario) {
+        super(nome, idade, email);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
