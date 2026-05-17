d487e6c37dea38e611cd895f3d8ff6994e261d02
Joao Felipe <joaolipepe11@gmail.com>
2026-05-17 17:00:56 -0300

    main ainda precisa de ajustes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        // USUÁRIOS
+
         Usuario u = new Usuario("João", "123", "senha", 20, "M", "email",
                 "Joao", "1111", "123");
 
@@ -10,23 +12,56 @@ public class Main {
         Critico critico = new Critico("Leo Dias", "555", "fofoca", 45, "M", "email",
                 "leo", "1234", "554", "Globo");
 
-        Filme f = new Filme("Batman", 20.0, 120, "Filme do Batman");
-        Filme f2 = new Filme("Orgulho e Preconceito", 20.0, 130, "Romance clássico");
+        Funcionario funcionario = new Funcionario( // FUNCIONÁRIO E ADMIN
+                "Carlos", 30, "carlos@email", 2500);
 
-        Sessao s = new Sessao(f, 1, TipoSala.TRES_D, "18:00 - 20:00");
-        Sessao s2 = new Sessao(f2, 2, TipoSala.COMUM, "20:00 - 22:00");
+        Administrador admin = new Administrador(
+                "Ana", 40, "ana@email", 5000, 1);
 
-        // Usando sala
-        Sala sala = new Sala(5);
+        // FILMES
+        Filme f = new Filme("Batman", 20.0, 120, "Filme do Batman", true);
+
+        Filme f2 = new Filme("Orgulho e Preconceito", 20.0, 130, "Romance clássico", false);
+
+        // SESSÕES
+        Sessao s = new Sessao(f, 1, TipoSala.TRES_D, "18:00 - 20:00", true);
+
+        Sessao s2 = new Sessao(f2, 2, TipoSala.COMUM, "20:00 - 22:00", false);
+
+        // SALA
+       Sala sala = new Sala(5);
+
         sala.adicionarSessao(0, s);
         sala.adicionarSessao(1, s2);
 
-        // Mostrar Sessoes
+        
         sala.mostrarSessoes();
 
+        funcionario.adicionarUsuario(u);
+        funcionario.alterarUsuario(e);
+
+        admin.adicionarUsuario(u);
+        admin.alterarUsuario(e);
+
+
+        admin.excluirUsuario(e); // metodo do admin
+
+
+        funcionario.incluirFilme(f); //metodos da interface
+
+        admin.alterarFilme(f2);
+
+        admin.excluirFilme(f);
+
+        // BILHETES
+
         Bilhete b1 = new Bilhete(u, s, 2, 5);
+
         Bilhete b2 = new Bilhete(e, s, 2, 6);
+
         Bilhete b3 = new Bilhete(critico, s2, 3, 4);
+
+        // COMPRA
 
         Compra c = new Compra();
 
@@ -37,7 +72,8 @@ public class Main {
         c.adicionarProduto(Produto.PIPOCA);
         c.adicionarProduto(Produto.REFRIGERANTE);
 
-        // críticas
+        // CRÍTICAS
+
         critico.atribuirNota(10, f);
         critico.atribuirCritica("Filme muito bom.", f);
 
@@ -47,23 +83,43 @@ public class Main {
         critico.atribuirNota(7, f2);
         critico.atribuirCritica("Bom, mas um pouco lento.", f2);
 
-        // exibição
         c.mostrarCompra();
 
+
+        
+        // EXIBIÇÃO DOS FILMES
+
         System.out.println("\n===== FILME 1 =====");
+
         System.out.println("Nome: " + f.getNome());
+
         System.out.println("Nota média: " + f.getNota());
+
         System.out.println("Quantidade de críticos: " + f.getQuantidadeCriticos());
+
+        System.out.println("Críticas:");
+
         f.mostrarCriticas();
 
+
         System.out.println("\n===== FILME 2 =====");
+
         System.out.println("Nome: " + f2.getNome());
+
         System.out.println("Nota média: " + f2.getNota());
+
         System.out.println("Quantidade de críticos: " + f2.getQuantidadeCriticos());
+
+        System.out.println("Críticas:");
+
         f2.mostrarCriticas();
 
-        System.out.println("\nTotal normal: R$ " + c.calcularTotal());
-        System.out.println("Total com desconto: R$ " +
-                c.calcularTotal(CupomPromocional.DESCONTO10));
+        // TOTAL DA COMPRA
+
+        System.out.println("\nTotal normal: R$ "
+                + c.calcularTotal());
+
+        System.out.println("Total com desconto: R$ "
+                + c.calcularTotal(CupomPromocional.DESCONTO10));
     }
 }
